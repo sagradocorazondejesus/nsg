90d66a3e440b802680104902b07d05913045176c
Sagrado Corazón de Jesús <kerigmanuevomilagrodeamor@gmail.com>
2026-02-26 17:35:29 -0600

    Update vibration pattern on msgCard click
    Change vibration pattern on click event to a heartbeat effect.

--- cuaresma/script.js
+++ cuaresma/script.js
@@ -3,7 +3,8 @@ const msgCard = document.getElementById("msgCard");
 msgCard.addEventListener("click", () => {
   msgCard.classList.toggle("flipped");
 
+  // Vibración corta al tocar (si el dispositivo lo permite)
   if (navigator.vibrate) {
-    navigator.vibrate(30);
+    navigator.vibrate([40, 40, 40]); // (latido)
   }
 });
